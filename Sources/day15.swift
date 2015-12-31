a800e5a2f1706d5dcaca99ae78198b4619d6c3a6
Matthew <matthewh806@gmail.com>
2015-12-31 23:46:41 +0100

    Add solution for day15

--- Sources/day15.swift
+++ Sources/day15.swift
@@ -9,56 +9,74 @@
 import Foundation
 
 class Day15: Day {
-    let input = try! String(contentsOfFile:"/Users/matthew/Projects/swift/advent-of-code/AdventOfCode/AdventOfCode/Input/day15.txt", encoding: NSUTF8StringEncoding)
+    let input = try! String(contentsOfFile:"/Users/matthew/Projects/swift/AdventOfCode/AdventOfCode/Input/day15.txt", encoding: NSUTF8StringEncoding)
     
     func partOne() -> String {
         var ingredients: [Ingredient] = []
         input.componentsSeparatedByString("\n").forEach({ ingredients.append(Ingredient(fromString: $0))})
-        
-        for ingredient in ingredients {
-            print(ingredient.name)
-        }
-        
-        return ""
+        return "\(getBestCookieScore(ingredients, numTeaspoons: 100))"
     }
     
     func partTwo() -> String {
-        return ""
+        var ingredients: [Ingredient] = []
+        input.componentsSeparatedByString("\n").forEach({ ingredients.append(Ingredient(fromString: $0))})
+        return "\(getBestCookieScore(ingredients, numTeaspoons: 100, numCalories: 500))"
+
     }
     
     func testCode() -> String {
         let butterscotch = Ingredient(fromString: "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8")
         let cinnamon = Ingredient(fromString: "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3")
-        
-        return "\(getBestRecipe([butterscotch, cinnamon]).totalScore)"
+        return "\(getBestCookieScore([butterscotch, cinnamon], numTeaspoons: 100, numCalories: 500))"
     }
     
-    func getBestRecipe(ingredientsArray: [Ingredient]) -> Recipe {
-        let numIngredients = ingredientsArray.count
-        let numTeaspoons = 100
-        let maxScore = 0
-        
-        //var ingredients: [Ingredient: Int] = [butterscotch: 0, cinnamon: 0]
-        var ingredients: [Ingredient: Int] = [:]
-        for ingredient in ingredientsArray {
-            ingredients[ingredient] = 0
-        }
-        //ingredients.forEach({ print( $0.0.name ) })
-        
-        //Test all different combinations
-        while(ingredients.reduce(0) {$0 + $1.1} < 100) {
-            ingredients[(ingredients.first?.0)!]! += 1
-        }
-        
-        return Recipe(ingredients: ingredients)
+    func getBestCookieScore(ingredientsArray: [Ingredient], numTeaspoons: Int, numCalories: Int = 0) -> Int {
+        return numCalories == 0 ? distributeTeaspoons([:], remainingTeaspoons: numTeaspoons, remainingIngredients: ingredientsArray).map({ Cookie.init(ingredients: $0).totalScore}).maxElement()! : (distributeTeaspoons([:], remainingTeaspoons: numTeaspoons, remainingIngredients: ingredientsArray).map({ Cookie.init(ingredients: $0) } ).filter({ $0.getCalories == 500 }).maxElement({ $0.totalScore < $1.totalScore })?.totalScore)!
     }
     
-    class Recipe {
+    func distributeTeaspoons(ingredients: [Ingredient:Int], remainingTeaspoons: Int) -> [[Ingredient:Int]] {
+        if remainingTeaspoons == 0 {
+            return [ingredients]
+        } else {
+            var arrangements: [[Ingredient:Int]] = []
+            for (k, _) in ingredients {
+                var newIngredients = ingredients
+                newIngredients[k]! += 1
+                arrangements += distributeTeaspoons(newIngredients, remainingTeaspoons: remainingTeaspoons-1)
+            }
+            return arrangements
+        }
+    }
+    
+    func distributeTeaspoons(ingredients: [Ingredient:Int], remainingTeaspoons: Int, remainingIngredients: [Ingredient]) -> [[Ingredient:Int]] {
+        if remainingIngredients.count == 1 {
+            var newIngredients = ingredients
+            newIngredients[remainingIngredients[0]] = remainingTeaspoons
+            return [newIngredients]
+        } else {
+            var arrangements: [[Ingredient:Int]] = []
+            for i in 0...remainingTeaspoons {
+                var newIngredients = ingredients
+                var stillRemainingIngredients = remainingIngredients
+                let nextIngredient = stillRemainingIngredients.removeFirst()
+                newIngredients[nextIngredient] = i
+                arrangements += distributeTeaspoons(newIngredients, remainingTeaspoons: remainingTeaspoons-i, remainingIngredients: stillRemainingIngredients)
+            }
+            
+            return arrangements
+        }
+    }
+    
+    class Cookie{
         let MAX_INGREDIENTS = 100
         var ingredients: [Ingredient: Int]
         
         init(ingredients: [Ingredient: Int]) {
             self.ingredients = ingredients
+        }
+        
+        var getCalories: Int {
+            return ingredients.reduce(0, combine: { $0 + ($1.0.calories * $1.1) })
         }
         
         var totalScore: Int {
@@ -77,7 +95,6 @@ class Day15: Day {
                 if(flavor < 0) { flavor = 0 }
                 if(texture < 0) { texture = 0 }
                 
-                print("Capacity: \(capacity), Durability: \(durability), Flavor: \(flavor), Texture: \(texture)")
                 return (capacity * durability * flavor * texture)
             }
         }
