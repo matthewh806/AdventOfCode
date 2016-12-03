331a3c31072f7561101703e8b9eee5f634120556
Matthew <matthewh806@gmail.com>
2016-12-03 23:00:48 +0100

    day 2 part 2 solved

--- 2016/python/advent-of-code-2016/day_02.py
+++ 2016/python/advent-of-code-2016/day_02.py
@@ -1,6 +1,6 @@
 from definitions import INPUT_DIR
 
-legal_moves = {
+legal_moves_1 = {
     '1': {'R':'2', 'D':'4'},
     '2': {'L':'1', 'R':'3', 'D':'5'},
     '3': {'L':'2', 'D':'6'},
@@ -12,9 +12,27 @@ legal_moves = {
     '9': {'U':'6', 'L':'8'}
 }
 
-def parse_instructions(data):
+legal_moves_2 = {
+    '1': {'D':'3'},
+    '2': {'R':'3', 'D':'6'},
+    '3': {'U':'1', 'L':'2', 'R':'4', 'D':'7'},
+    '4': {'L':'3', 'D':'8'},
+    '5': {'R':'6'},
+    '6': {'U':'2', 'L':'5', 'D':'A', 'R':'7'},
+    '7': {'U':'3', 'L':'6', 'D':'B', 'R':'8'},
+    '8': {'U':'4', 'L':'7', 'D':'C', 'R':'9'},
+    '9': {'L':'8'},
+    'A': {'U':'6', 'R':'B'},
+    'B': {'U':'7', 'L':'A', 'D':'D', 'R':'C'},
+    'C': {'U':'8', 'L':'B'},
+    'D': {'U':'B'}
+}
+
+def parse_instructions(data, part=1):
     pos = '5'
     code = []
+
+    legal_moves = legal_moves_1 if part==1 else legal_moves_2
 
     for i in data:
         instr = i.rstrip('\n')
@@ -35,5 +53,8 @@ if __name__ == "__main__":
 
     print parse_instructions(test_input)
     print parse_instructions(input_data)
+
+    print parse_instructions(test_input, 2)
+    print parse_instructions(input_data, 2)
 
 
