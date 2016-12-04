335e928805045e3dbd550d26d17cb0379af63036
Matthew <matthewh806@gmail.com>
2016-12-04 13:58:20 +0100

    day 4 part 1 solution

--- 2016/python/advent_of_code_2016/day_04.py
+++ 2016/python/advent_of_code_2016/day_04.py
@@ -1,10 +1,29 @@
 from definitions import INPUT_DIR
+from collections import Counter
+import re
 
+def get_n_most_common_chars(text, n):
+    freq = Counter(text).most_common()
+    return "".join([c for (c, f) in sorted(freq, key=lambda x: (-x[1],
+                                                                x[0]))][0:n])
 def is_room_real(data):
-    return True
+    encrypted_name, _, checksum = parse_room_data(data)
+    most_freq = get_n_most_common_chars(encrypted_name.replace('-', ''), 5)
+    
+    return most_freq == checksum
+
+def parse_room_data(data):
+    sector_id = re.findall(r'\d+', data)[0]
+    checksum = re.findall(r'\[(.*?)\]', data)[0]
+    encrypted_name = re.split(r'-\d+', data)[0]
+
+    return encrypted_name, int(sector_id), checksum
+
+def get_sector_id_sum(data):
+    return sum([parse_room_data(d)[1] for d in data if is_room_real(d)])
 
 if __name__ == "__main__":
     with open(INPUT_DIR + 'day_04.txt') as f:
         input_data = [line.rstrip('\n') for line in f]
 
-    print input_data
+    print get_sector_id_sum(input_data)
--- 2016/python/tests/test_day_04.py
+++ 2016/python/tests/test_day_04.py
@@ -1,12 +1,29 @@
-from advent_of_code_2016.day_04 import is_room_real
+from advent_of_code_2016.day_04 import is_room_real, parse_room_data,\
+                                        get_n_most_common_chars, \
+                                        get_sector_id_sum
+
+t1 = 'aaaaa-bbb-z-y-x-123[abxyz]'
+t2 = 'a-b-c-d-e-f-g-h-987[abcde]'
+t3 = 'not-a-real-room-404[oarel]'
+t4 = 'totally-real-room-200[decoy]'
+
+def test_parse_room_data():
+    encrypted_name, sector_id, checksum = parse_room_data(t1)
+
+    assert encrypted_name == 'aaaaa-bbb-z-y-x'
+    assert sector_id == 123
+    assert checksum == 'abxyz'
+
+def test_get_n_most_common_chars():
+    assert get_n_most_common_chars('aaaaabbbzyx', 5) == 'abxyz'
+    assert get_n_most_common_chars('notarealroom', 5) == 'oarel'
 
 def test_is_room_real():
-    t1 = 'aaaaa-bbb-z-y-x-123[abxyz]'
-    t2 = 'a-b-c-d-e-f-g-h-987[abcde]'
-    t3 = 'not-a-real-room-404[oarel]'
-    t4 = 'totally-real-room-200[decoy]'
-
     assert is_room_real(t1)
     assert is_room_real(t2)
     assert is_room_real(t3)
-    assert is_room_real(t4)
+    assert not is_room_real(t4)
+
+def test_get_sector_id_sum():
+    input = [t1, t2, t3, t4]
+    assert get_sector_id_sum(input) == 1514
