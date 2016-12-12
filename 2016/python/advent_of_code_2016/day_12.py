1ba48c099d3c45c594ba19f313defeb91bdb6d1b
Matthew <matthewh806@gmail.com>
2016-12-13 00:58:10 +0100

    solve day 12 part 2

--- 2016/python/advent_of_code_2016/day_12.py
+++ 2016/python/advent_of_code_2016/day_12.py
@@ -41,7 +41,10 @@ if __name__ == "__main__":
     with open(INPUT_DIR + 'day_12.txt') as f:
         instr = [l.rstrip('\n') for l in f]
 
-    ab = AssemBunny({'a':0,'b':0,'c':0,'d':0})
-    ab.parse_instructions(instr)
-    print ab.registers
+    p1 = AssemBunny({'a':0,'b':0,'c':0,'d':0})
+    p2 = AssemBunny({'a':0,'b':0,'c':1,'d':0})
+    p1.parse_instructions(instr)
+    p2.parse_instructions(instr)
+    print p1.registers
+    print p2.registers
 
