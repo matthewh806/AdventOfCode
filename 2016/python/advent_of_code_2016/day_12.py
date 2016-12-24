86080f8bc53bb31dc26fd33753b10c5bc7553fb7
Matthew <matthewh806@gmail.com>
2016-12-24 19:32:19 +0100

    Move assembunny class to separate file

--- 2016/python/advent_of_code_2016/day_12.py
+++ 2016/python/advent_of_code_2016/day_12.py
@@ -1,41 +1,5 @@
 from definitions import INPUT_DIR
-import re
-
-class AssemBunny():
-    def __init__(self, registers):
-        self.registers = registers
-
-    def copy(self, instr):
-        _, v, reg = instr.split(' ')
-        self.registers[reg] = int(v) if str.isdigit(v) else self.registers[v] 
-        
-    def increment(self, instr):
-        _, reg = instr.split(' ')
-        self.registers[reg] += 1 
-
-    def decrement(self, instr):
-        _, reg = instr.split(' ')
-        self.registers[reg] -= 1 
-
-    def parse_instructions(self, instr):
-        idx = 0
-        while idx < len(instr):
-            i = instr[idx]
-            
-            if i.startswith('cpy'):
-                self.copy(i)
-            elif i.startswith('inc'):
-                self.increment(i)
-            elif i.startswith('dec'):
-                self.decrement(i)
-            elif i.startswith('jnz'):
-                _, reg, offset = i.split(' ')
-
-                if str.isdigit(reg) or self.registers[reg] != 0:
-                    idx+=int(offset)
-                    continue
-
-            idx=idx+1
+from assembunny import AssemBunny
 
 if __name__ == "__main__":
     with open(INPUT_DIR + 'day_12.txt') as f:
