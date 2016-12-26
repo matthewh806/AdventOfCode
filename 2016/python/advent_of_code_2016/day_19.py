d158f7e1dd4159c681783511143932233005ced1
Matthew <matthewh806@gmail.com>
2016-12-26 11:57:40 +0100

    Solve day 19 part 2 with highly ineff. soln.

--- 2016/python/advent_of_code_2016/day_19.py
+++ 2016/python/advent_of_code_2016/day_19.py
@@ -1,15 +1,23 @@
 def elves(num):
-    return range(1, num+1)
+    return list(range(1, num+1))
 
-def winner(elves):
+def winner(elves, fn):
     while len(elves) > 1:
-       elves = round(elves)
+       elves = fn(elves)
     return elves[0] 
 
-def round(elves):
+def round_part_one(elves):
     return elves[::2] if len(elves) % 2 == 0 else elves[2::2]
+
+def round_part_two(elves):
+    idx = 0
+    while idx < len(elves):
+        elves.pop((idx + len(elves) / 2) % len(elves))
+        idx+=1
+    return elves
 
 if __name__ == "__main__":
     puzzle_input = 3005290
 
-    print winner(elves(puzzle_input))
+    print winner(elves(puzzle_input), round_part_one)
+    print winner(elves(puzzle_input), round_part_two)
