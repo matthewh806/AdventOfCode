0f2df1da3b89e9ac98ba99b7af0b9956b50796f6
Matthew <matthewh806@gmail.com>
2016-12-26 12:50:18 +0100

    day 19 pt.2 more efficient

--- 2016/python/advent_of_code_2016/day_19.py
+++ 2016/python/advent_of_code_2016/day_19.py
@@ -1,3 +1,5 @@
+import math
+
 def elves(num):
     return list(range(1, num+1))
 
@@ -10,11 +12,14 @@ def round_part_one(elves):
     return elves[::2] if len(elves) % 2 == 0 else elves[2::2]
 
 def round_part_two(elves):
-    idx = 0
-    while idx < len(elves):
-        elves.pop((idx + len(elves) / 2) % len(elves))
-        idx+=1
-    return elves
+    elim = 0
+    N = len(elves)
+    for i in range(int(math.ceil(N / 3.0))):
+        rem = i + elim + N / 2
+        elves[rem] = None
+        elim += 1
+        N -= 1
+    return list(filter(None, elves[i+1:] + elves[:i+1]))
 
 if __name__ == "__main__":
     puzzle_input = 3005290
