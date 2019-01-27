0bc9d725af1bece71dccefad7bd818d5d3f102e1
matthewh806 <matthewh806@gmail.com>
2019-01-27 12:34:18 +0100

    day_01 part 1 complete

---
+++ 2018/c++/BelatedAdvent/BelatedAdvent/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  BelatedAdvent
+//
+//  Created by Matthew on 26/01/2019.
+//  Copyright © 2019 Matthew. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+int caclulate_resulting_frequency(const std::vector<int> in_operations, int f_0 = 0) {
+    for(auto o : in_operations) {
+        f_0 += o;
+    }
+    
+    return f_0;
+}
+
+int main(int argc, const char * argv[]) {
+    std::vector<int> operations;
+    
+    int starting_freq = 0; // Hz <--- does it matter? xD
+    std::string line;
+    std::ifstream input("input_01.txt");
+    
+    // TODO: Abstract away reading the input
+    if(!input.is_open()) {
+        std::cout << "Error opening input file!" << std::endl;
+        return 0;
+    }
+    
+    int num_lines = 0;
+    while(std::getline(input, line)) {
+        operations.push_back(std::stoi(line, nullptr));
+        num_lines++;
+    }
+    
+    std::cout << "No. lines: " << num_lines << std::endl;
+    std::cout << "Final frequency: " << caclulate_resulting_frequency(operations, starting_freq) << std::endl;
+    
+    // TODO: Remember to close the file!
+    input.close();
+    
+    return 0;
+}
